260dd0f88602e50222b7b6362834886f09158ed6
Mark Schisler <mark.schisler@knowink.com>
2020-07-31 18:18:40 -0500

    cleaning up mapview

--- WeatherApp/MainView/MapView.swift
+++ WeatherApp/MainView/MapView.swift
@@ -1,36 +1,6 @@
 import SwiftUI
 import MapKit
 
-class MapViewDelegate: NSObject, MKMapViewDelegate {
-    let renderer: MKTileOverlayRenderer
-    
-    init(renderer: MKTileOverlayRenderer) {
-        self.renderer = renderer
-    }
-    
-    func mapView(
-      _ mapView: MKMapView,
-      rendererFor overlay: MKOverlay
-    ) -> MKOverlayRenderer {
-      return renderer
-    }
-}
-
-var tileOverlay: MKTileOverlay = {
-    let tileOverlay = MKTileOverlay(urlTemplate: "https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=\(Constants.API_KEY)")
-    tileOverlay.minimumZ = 2
-    tileOverlay.maximumZ = 16
-    tileOverlay.canReplaceMapContent = false
-    return tileOverlay
-}()
-
-var delegate: MapViewDelegate = {
-    let tileRenderer = MKTileOverlayRenderer(tileOverlay: tileOverlay)
-    tileRenderer.alpha = 0.8
-    return MapViewDelegate(renderer: tileRenderer)
-}()
-
-
 struct MapView: UIViewRepresentable {
     var coordinate: CLLocationCoordinate2D
 
@@ -43,14 +13,43 @@ struct MapView: UIViewRepresentable {
     }
 
     func updateUIView(_ uiView: MKMapView, context: Context) {
-        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        let span = MKCoordinateSpan(latitudeDelta: 0.7, longitudeDelta: 0.7)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         uiView.setRegion(region, animated: true)
     }
 }
+
+private class MapViewDelegate: NSObject, MKMapViewDelegate {
+    private let renderer: MKTileOverlayRenderer
+    
+    init(renderer: MKTileOverlayRenderer) {
+        self.renderer = renderer
+    }
+    
+    func mapView(_ mapView: MKMapView,rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+      return renderer
+    }
+}
+
+private var tileOverlay: MKTileOverlay = {
+    let tileOverlay = MKTileOverlay(urlTemplate: "https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=\(Constants.API_KEY)")
+    tileOverlay.minimumZ = 2
+    tileOverlay.maximumZ = 16
+    tileOverlay.canReplaceMapContent = false
+    return tileOverlay
+}()
+
+private var delegate: MapViewDelegate = {
+    let tileRenderer = MKTileOverlayRenderer(tileOverlay: tileOverlay)
+    tileRenderer.alpha = 0.8
+    return MapViewDelegate(renderer: tileRenderer)
+}()
+
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView(coordinate: Constants.START_LOCATION)
     }
 }
+
+
