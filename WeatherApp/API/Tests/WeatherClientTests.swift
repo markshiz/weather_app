93ab4c3807793c527dba59cb9df3f6f5a1f45a9e
Mark Schisler <mark.schisler@knowink.com>
2020-07-31 10:47:48 -0500

    adding more api tests

--- WeatherApp/API/Tests/WeatherClientTests.swift
+++ WeatherApp/API/Tests/WeatherClientTests.swift
@@ -31,8 +31,38 @@ class WeatherClientTests: XCTestCase {
         let elements = try recorder.elements.get()
         XCTAssertTrue(elements[0].list.count > 0)
     }
+    
+    func testWeatherCitySearch() throws {
+        let recorder = try testEffect(effect: testObject.weather(query: .cityName("saint louis")))
+        let elements = try recorder.elements.get()
+        XCTAssertNotNil(elements[0].name)
+    }
 
-    private func testEffect<D : Decodable, E : Error>(effect: Effect<D, E>) throws -> Recorder<D, E> {
+    func testWeatherZipSearch() throws {
+        let recorder = try testEffect(effect: testObject.weather(query: .zipCode("63109")))
+        let elements = try recorder.elements.get()
+        XCTAssertNotNil(elements[0].name)
+    }
+    
+    func testWeatherLatLongSearch() throws {
+        let recorder = try testEffect(effect: testObject.weather(query: .latitudeLongitude(38, -90)))
+        let elements = try recorder.elements.get()
+        XCTAssertNotNil(elements[0].name)
+    }
+    
+    func testWeatherCityStateSearch() throws {
+        let recorder = try testEffect(effect: testObject.weather(query: .cityAndState("chicago", "illinois")))
+        let elements = try recorder.elements.get()
+        XCTAssertNotNil(elements[0].name)
+    }
+    
+    func testImage() throws {
+        let recorder = try testEffect(effect: testObject.conditionImageFromString(string: "10d"))
+        let elements = try recorder.elements.get()
+        XCTAssertNotNil(elements[0])
+    }
+
+    private func testEffect<D, E : Error>(effect: Effect<D, E>) throws -> Recorder<D, E> {
         let recorder = effect.record()
         let completion = try wait(for: recorder.completion, timeout: 5)
         if case let .failure(error) = completion {
